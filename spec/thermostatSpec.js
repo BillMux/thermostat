c0649c3d5be3d8b0b6c5f68881cdb5c0a85faed6
Caitlin Cooling <ccooling97@gmail.com>
2018-10-30 15:04:47 +0000

    Power save functionality to find max temp

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -5,17 +5,16 @@ describe("Thermostat", function(){
     thermostat = new Thermostat();
   });
 
-// Thermostat starts at 20 degrees
   it("Starts at 20 degrees", function(){
     expect(thermostat.displayTemp()).toEqual(20)
   });
 
-// You can increase the temperature with an up function
   describe("change temp methods", function(){
     it("increases temperature when we call up", function(){
       thermostat.up(3)
       expect(thermostat.displayTemp()).toBe(23)
     });
+
     it("reduces temperature when we call down", function(){
       thermostat.down(3)
       expect(thermostat.displayTemp()).toBe(17)
@@ -26,10 +25,28 @@ describe("Thermostat", function(){
     it("doesn't let temperature below 10 degrees", function() {
       thermostat.down(11)
       expect(thermostat.displayTemp()).toBe(10)
-    })
-    it("doesn't let temperature above 25 degrees", function() {
+    });
+
+    it("doesn't let temperature above 25 degrees if powersave is on", function() {
       thermostat.up(11)
       expect(thermostat.displayTemp()).toBe(25)
-    })
-  })
+    });
+
+    it("doesn't let temperature above 32 degrees if powersave is off", function() {
+      thermostat.turnOffPowerSave();
+      thermostat.up(15)
+      expect(thermostat.displayTemp()).toBe(32)
+    });
+  });
+
+  describe("power saving mode", function() {
+    it("is on by default", function(){
+      expect(thermostat.displayPowerSave()).toBe(true)
+    });
+
+    it("can be turned off", function(){
+      thermostat.turnOffPowerSave();
+      expect(thermostat.displayPowerSave()).toBe(false)
+    });
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,13 +1,16 @@
 var Thermostat = function(){
   this.temp = 20
+  this.powerSave = true
 };
 
 Thermostat.prototype.displayTemp = function () {
   if(this.temp < 10) {
     this.temp = 10;
-  }
-  if(this.temp > 25) {
+  } else if(this.temp > 25 && this.powerSave === true) {
     this.temp = 25;
+  }
+  else if(this.temp > 32 && this.powerSave === false) {
+    this.temp = 32;
   }
   return this.temp
 };
@@ -18,4 +21,12 @@ Thermostat.prototype.up = function (amount) {
 
 Thermostat.prototype.down = function (amount) {
   this.temp -= amount
+};
+
+Thermostat.prototype.displayPowerSave = function () {
+  return this.powerSave
+};
+
+Thermostat.prototype.turnOffPowerSave = function () {
+  this.powerSave = false
 };
